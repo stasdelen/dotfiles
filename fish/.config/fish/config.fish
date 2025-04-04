384cc5d0c70351b1de34a7e5af982bd4f56b156e
stasdelen <salih.tasdelen@hotmail.com>
2025-04-04 16:12:06 +0300

    change cursor with mode

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Set cursor shapes for different modes
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_replace underscore
+set fish_cursor_external line
+set fish_cursor_visual block

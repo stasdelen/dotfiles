7614d5205ccdc14e18a13288bfcc64b766664568
SalihTasdelen <salih.tasdelen@hotmail.com>
2025-12-18 14:40:36 +0300

    Update telescope version

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -380,7 +380,7 @@ require('lazy').setup({
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
+    tag = 'v0.2.0',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
@@ -1001,6 +1001,7 @@ require('lazy').setup({
         'python',
         'diff',
         'json',
+        'jsonc',
         'html',
         'xml',
         'yaml',
